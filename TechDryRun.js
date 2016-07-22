5c3293f94d52b8532bfc731779bfdfa9dd5dd8cb
Michael Olorunnisola <sweetpianist1@gmail.com>
2016-07-21 22:25:57 -0400

    It Works

---
+++ TechDryRun.js
@@ -0,0 +1,48 @@
+//Tech Dry Run
+
+var hrr18 = {
+  name: 'Keeley Nakamoto',
+  anothername: 'Jim',
+  jeffName: 'Jeff Bernstein',
+  yetAnother: 'Dan jrrn eerr n nnrr rner rnnr  eees n oynllolonyro ynronllooo loo
+
+ ro
+nnr
+
+ name: 'Caleb',
+
+  neekon: neekon,
+  per
+
+so
+nskf;lasdkf;lasdkf
+
+  na
+myyyydddd
+
+//John Test hi
+
+//Jason Here! good to go.
+
+
+
+yy
+PayPe
+kdkdkdkdkdk hello
+
+// STEPHENIE CHANGES -- //
+
+y: 'PPayt:yy}:::
+:
+yylo
+ama
+eeeaeema:meeaamaeaaeeaeaaeeeeeeee:e::eee emmea::a esnY
+
+hello
+Hello
+
+
+
+sa
+name: 'Mchael Olorunnisola'
+}
